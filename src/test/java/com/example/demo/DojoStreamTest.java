99025306914ea4a31bde84fada14b11138f2fa29
Jhonatan.garcia <jhonatan.garcia@sofka.com.co>
2023-10-27 14:05:54 -0500

    first commit

--- src/test/java/com/example/demo/DojoStreamTest.java
+++ src/test/java/com/example/demo/DojoStreamTest.java
@@ -4,27 +4,34 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class DojoStreamTest {
 
     @Test
-    void converterData(){
+    void converterData() {
         List<Player> list = CsvUtilFile.getPlayers();
         assert list.size() == 18207;
     }
 
     @Test
-    void jugadoresMayoresA35(){
+    void jugadoresMayoresA35() {
+
+//        Predicate evaluador = (Object jugador) -> {
+//            Player jugadorCasteado = (Player)jugador;
+//            return jugadorCasteado.getAge() > 35;
+//        };
+
         List<Player> list = CsvUtilFile.getPlayers();
-        Set<Player> result = list.stream()
-                .filter(jugador -> jugador.getAge() > 35)
+        Set<Player> result = (Set<Player>) list.stream()
+                .filter(player -> player.getAge() > 35)
                 .collect(Collectors.toSet());
         result.forEach(System.out::println);
     }
 
     @Test
-    void jugadoresMayoresA35SegunClub(){
+    void jugadoresMayoresA35SegunClub() {
         List<Player> list = CsvUtilFile.getPlayers();
         Map<String, List<Player>> result = list.stream()
                 .filter(player -> player.getAge() > 35)
@@ -36,28 +43,99 @@ public class DojoStreamTest {
             System.out.println(key + ": ");
             jugadores.forEach(System.out::println);
         });
+    }
+
+    @Test
+    void mejorJugadorConNacionalidadFrancia() {
+
+        List<Player> list = CsvUtilFile.getPlayers();
+        Optional<Player> result = list.stream()
+                .filter(jugador -> "France".equals(jugador.getNational()))
+                .max((j1, j2) -> Integer.compare(j1.getWinners(), j2.getWinners()));
+
+        if (result.isPresent()) {
+            Player maxWinner = result.get();
+            System.out.println("Jugador con más winners de Francia: " + maxWinner.getName() +
+                    " con " + maxWinner.getWinners() + " victorias.");
+        } else {
+            System.out.println("No se encontró ningún jugador de Francia.");
+        }
+    }
+
+
+    @Test
+    void clubsAgrupadosPorNacionalidad() {
+        List<Player> list = CsvUtilFile.getPlayers();
+        /*
+        Map<String, List<String>> result = list.stream()
+                .distinct()
+                .collect(Collectors.groupingBy(Player::getNational, Collectors.mapping(Player::getClub, Collectors.toList())));
+
+        result.forEach((key, jugadores) -> {
+            System.out.println("\n");
+            System.out.println(key + ": ");
+            jugadores.forEach(System.out::println);
+        });*/
+
+
+        // Agrupar jugadores por nacionalidad
+        Map<String, List<Player>> clubesAgrupadosPorNacionalidad = list.stream()
+                .collect(Collectors.groupingBy(Player::getNational));
+
+        // Mostrar los clubes de cada nacionalidad
+        clubesAgrupadosPorNacionalidad.forEach((nacionalidad, listaJugadores) -> {
+            System.out.println("Nacionalidad: " + nacionalidad);
+            System.out.println("Clubes: " + listaJugadores.stream().map(Player::getClub).collect(Collectors.toList()));
+        });
+    }
+
+    @Test
+    void clubConElMejorJugador() {
+
+        //nop ni idea xd
+        List<Player> list = CsvUtilFile.getPlayers();
+        String clubConMasWinners = list.stream()
+                .collect(Collectors.groupingBy(Player::getClub, Collectors.summingInt(Player::getWinners)))
+                .entrySet().stream()
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .map(Map.Entry::getKey)
+                .orElse(null);
+
+        System.out.println("Club con más winners: " + clubConMasWinners);
 
     }
 
     @Test
-    void mejorJugadorConNacionalidadFrancia(){
-    }
+    void ElMejorJugador() {
 
 
-    @Test
-    void clubsAgrupadosPorNacionalidad(){
-    }
+        List<Player> list = CsvUtilFile.getPlayers();
+        Optional<Player> result = list.stream()
+                .max((j1, j2) -> Integer.compare(j1.getWinners(), j2.getWinners()));
 
-    @Test
-    void clubConElMejorJugador(){
-    }
+            Player maxWinner = result.get();
+            System.out.println("Jugador con más winners: " + maxWinner.getName() +
+                    " con " + maxWinner.getWinners() + " victorias.");
 
-    @Test
-    void ElMejorJugador(){
     }
 
     @Test
-    void mejorJugadorSegunNacionalidad(){
+    void mejorJugadorSegunNacionalidad() {
+
+
+        List<Player> list = CsvUtilFile.getPlayers();
+
+        Map<String, Optional<Player>> maxWinnersPorNacionalidad = list.stream()
+                .collect(Collectors.groupingBy(Player::getNational,
+                        Collectors.maxBy(Comparator.comparing(Player::getWinners))));
+
+        // Mostrar el jugador con más winners por cada nacionalidad
+        maxWinnersPorNacionalidad.forEach((nacionalidad, jugador) -> {
+            jugador.ifPresent(j -> {
+                System.out.println("Nacionalidad: " + nacionalidad);
+                System.out.println("Jugador con más winners: " + j.getName() + " con " + j.getWinners() + " victorias.");
+            });
+        });
     }
 
 
